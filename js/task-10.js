6797b9370611f24bb25da605105be3bd7e1fb51a
Zendr <520530ol@ukr.net>
2023-03-24 16:16:10 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -22,9 +22,59 @@ function getRandomHexColor() {
 
 Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 */
+
 const numberBoxEl = document.querySelector('input[type="number"]');
 const bgCreateEl = document.querySelector('button[data-create]');
 const btnDestroyEl = document.querySelector('button[data-destroy]');
 const containerBoxesEl = document.querySelector('#boxes');
 const boxEl = document.querySelector('.div-item');
+
+let originalSize = 30;
+const nullSize = 0;
+
+// console.log(numberBoxEl);
+// console.log(bgCreateEl);
+// console.log(btnDestroyEl);
+// console.log(containerBoxesEl);
+
+bgCreateEl.addEventListener('click', createBoxes);
+btnDestroyEl.addEventListener('click', destroyBoxes);
+
+function onFindAmount() {
+let amount = numberBoxEl.value;
+// console.log('amount', amount); 
+  return amount;
+};  
+
+function createBoxes(amount) {
+  if (onFindAmount() >= 1) {
+   
+    let amount = onFindAmount();
+
+      for (let i = 1; i <= amount; i += 1) {
+        
+        const boxEl = document.createElement("div");
+
+        boxEl.style.width = `${originalSize}px`;
+        boxEl.style.height = `${originalSize}px`;
+        originalSize += 10;
+        const randomColor = getRandomHexColor();
+// console.log('randomColor', randomColor);
+        boxEl.style.backgroundColor = randomColor;
+// console.log(boxEl);
+        containerBoxesEl.append(boxEl);
+      } 
+    
+  } else
+  return alert('Введіть число!');
+};
+
+function destroyBoxes() {
+  containerBoxesEl.innerHTML = '';
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
